fe5b77f22582da1d05b19c60701f35702f904cdd
DrewProg <integration76@yandex.ru>
2021-06-08 22:24:41 +0500

    Update error handler

--- middlewares/ErrorHandlingMiddleware.js
+++ middlewares/ErrorHandlingMiddleware.js
@@ -4,5 +4,5 @@ module.exports = function (err, req, res, next) {
     if (err instanceof ApiError) { //если класс ошибки ApiError
         return res.status(err.status).json({message: err.message}) //то возвращаем на клиент ответ
     }
-    return res.status(500).json({message: "Unexpected Error! Blyat'"});
+    return res.status(500).json({message: "Unexpected Error!"});
 };
