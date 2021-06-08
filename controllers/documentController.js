e3de0929a4533f53cd930f3a696100ecd8b80822
DrewProg <integration76@yandex.ru>
2021-06-09 00:54:20 +0500

    Add getAll with filters and pagination

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -7,8 +7,14 @@ exports.create = async (req, res) => {
 }
 
 exports.getAll = async (req, res) => {
-    const doc = await Document.findAll();
-    return res.json(doc);
+    let { docSetId, limit, page } = req.query;
+    let docs;
+    page = page || 1; //страницы
+    limit = limit || 5; //количество элементов на странице
+    let offset = page * limit - limit; //отступ: сколько элементов нужно пропустить, чтобы попасть на нужную страницу
+    if (docSetId) docs = await Document.findAndCountAll({ where: { docSetId }, limit, offset });
+    else docs = await Document.findAndCountAll({ limit, offset });
+    return res.json(docs);
 }
 
 exports.getOne = async (req, res) => {
