b8be5ccf0ae69328e2951ffe1cdaf2af349a6443
DrewProg <integration76@yandex.ru>
2021-06-09 00:51:59 +0500

    Remove materialui

--- views/react-app-dev/src/components/LogForm.js
+++ views/react-app-dev/src/components/LogForm.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import '../styles/LogForm.css'
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import { Button, FormControl, TextField } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        margin: {
-            margin: theme.spacing(1),
-        },
-    },
-}));
 
 function LogForm() {
-    const [values, setValues] = React.useState({
-        isAccess: false,
-        showPassword: false
-    });
-    const classes = useStyles();
-    const handleClickShowPassword = () => {
-        setValues({ ...values, showPassword: !values.showPassword });
-    };
-
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
@@ -42,26 +13,7 @@ function LogForm() {
 
     return (
         <form className={classes.root} onSubmit={handleSubmit}>
-            <TextField required id="standard-basic" label="Логин" />
-            <FormControl required>
-                <InputLabel htmlFor="standard-adornment-password">Пароль</InputLabel>
-                <Input
-                    id="standard-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    endAdornment={
-                        <InputAdornment>
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                            >
-                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                />
-            </FormControl>
-            <Button type="submit" variant="contained">Вход</Button>
+
         </form>
     );
 }
