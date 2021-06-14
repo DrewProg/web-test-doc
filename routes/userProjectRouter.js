217422b51f28550ebbabaa90f3e061b674cd6706
DrewProg <integration76@yandex.ru>
2021-06-15 01:30:20 +0500

    Updated controllers according to new middlewares

--- routes/userProjectRouter.js
+++ routes/userProjectRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const router = new Router();
 const userProjectController = require('../controllers/userProjectController');
+const checkRoleMiddleware = require('../middlewares/checkRoleMiddleware');
 
-router.post('/', userProjectController.create);
+router.post('/', checkRoleMiddleware(['MAIN_TESTER']), userProjectController.create);
 router.get('/', userProjectController.getAll);
 router.get('/:id', userProjectController.getOne);
 
