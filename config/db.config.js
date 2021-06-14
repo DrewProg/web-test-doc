5f61febe65075dfc8685cd98ff0cedcc1486c742
DrewProg <integration76@yandex.ru>
2021-06-15 01:24:43 +0500

    Added defines for sequelize

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 module.exports = new Sequelize(
   process.env.DB_NAME, //название бд
   process.env.DB_USER,
@@ -6,6 +6,7 @@ module.exports = new Sequelize(
   {
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    define: { timestamps: false, underscored: true, freezeTableName: true }
   }
 );
