7b9cc0730456e5850fa74bf4b33a993eb02f12ec
DrewProg <integration76@yandex.ru>
2021-06-15 01:37:45 +0500

    Update top bar

--- views/react-app-dev/src/components/TopBar.js
+++ views/react-app-dev/src/components/TopBar.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../index';
+import { observer } from "mobx-react-lite";
+import { LOGIN_ROUTE } from '../utils/consts';
+import { Nav, Navbar, Button, Container } from 'react-bootstrap';
+import { NavLink, Redirect, useHistory } from 'react-router-dom';
+import '../styles/TopBar.css';
 
-function TopBar() {
-    return (<></>
+const TopBar = observer(() => {
+    const { user } = useContext(Context);
+    const history = useHistory()
+;    const logOut = () => {
+        user.setUser({});
+        user.setIsAuth(false);
+        history.push(LOGIN_ROUTE);
+    }
+
+    return (
+        <Navbar bg="dark" variant="dark" fixed="top">
+            <Container>
+                <NavLink to={LOGIN_ROUTE}><div className="nav-burger">☰</div></NavLink>
+                {!user.isAuth ?
+                    <Nav className="ml-auto">
+                        <Button variant="outline-light" onClick={() => history.push(LOGIN_ROUTE)}>Вход</Button>
+                    </Nav> :
+                    <>
+                        <div>{user.nickname}</div>
+                        <Button variant="outline-light" onClick={() => logOut()}>Выйти</Button>
+                    </>}
+            </Container>
+        </Navbar >
     );
-}
+});
 
 export default TopBar;
